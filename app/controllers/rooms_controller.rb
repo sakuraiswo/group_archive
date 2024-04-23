abb2f95e5c80928d9768069c08f8fc0aa9015c0c
sakuraiswo <sakuraisworks@gmail.com>
2024-04-23 11:02:29 +0900

    中間マイグレート、コントローラー、ルーティング

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,8 +3,26 @@ class RoomsController < ApplicationController
 
   def index; end
 
+  def new
+    @room = Room.new
+   end
+ 
+   def create
+     @room = Room.new(room_params)
+     if @room.save
+       redirect_to root_path
+     else
+       render :new
+     end
+   end
+ 
+ 
+   private
+ 
+   def room_params
+     params.require(:room).permit(:name, :group_memo, user_ids: [])
+   end
 
 
 
-  
 end
