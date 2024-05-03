25edd008dbeca9f47b9bb952385666a25ac218b2
sakuraiswo <sakuraisworks@gmail.com>
2024-05-03 23:27:57 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # ここを適切なオリジンに変更してください
+    origins 'https://group-archive.onrender.com'  # ここを適切なオリジンに変更してください
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization', 'X-CSRF-Token']
+      expose: ['Authorization', 'X-CSRF-Token', 'ETag']
   end
 end
