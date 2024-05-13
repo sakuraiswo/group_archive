eb29004e57acebace7f643d9bbe2c810c239d05e
sakuraiswo <sakuraisworks@gmail.com>
2024-05-13 17:49:30 +0900

    オリジン変更

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module GroupArchive
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://group-archive.onrender.com'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
